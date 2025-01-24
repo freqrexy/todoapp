6fd5f874cfbeae7670827336f8eac957f1137b53
freqrexy <bev.wooff@gmail.com>
2025-01-24 14:18:28 +0000

    third commit: error handling un-commented out, some css changes.

--- website/views.py
+++ website/views.py
@@ -13,7 +13,7 @@ def home():
 
 @my_view.route("/add", methods=["POST"])
 def add():
-#    try:
+    try:
         task = request.form.get("task")
         category = request.form.get("category")
         today = datetime.today()
@@ -27,9 +27,9 @@ def add():
         db.session.commit()
         message = "Task added successfully."
         return redirect(url_for("my_view.home",message=message))
-#    except:
-#        message = "There was an error adding your task.  Please try again."
-#        return redirect(url_for("my_view.home",message=message))
+    except:
+        message = "There was an error adding your task.  Please try again."
+        return redirect(url_for("my_view.home",message=message))
 
 @my_view.route("/edit/<todo_id>", methods=["POST"])
 def edit(todo_id):
